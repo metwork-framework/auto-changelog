4c23cd41a9d2569731fed3007561184b9fa37efd
metworkbot <marty.fabien@orange.fr>
2019-04-18 15:32:20 +0200

    feat: add include/exclude branches options

--- auto_changelog/__main__.py
+++ auto_changelog/__main__.py
@@ -7,6 +7,8 @@ Options:
     -r=REPO --repo=REPO     Path to the repository's root directory [Default: .]
     -t=TITLE --title=TITLE  The changelog's title [Default: Changelog]
     -r=REV --rev=REV        REV (see git-rev-parse) for commit listing [Default: master]
+    --include-branches=BRA  comma separated fnmatch pattern for including branches [Default: *]
+    --exclude-branches=BRA  comma separated fnmatch pattern for excluding branches [Default:]
     -T=REV --tag-filter=PAT tag filter pattern (see fnmatch) [Default: *]
     --dont-keep-unreleased  Don't keep unreleases commits
     -d=DESC --description=DESC
@@ -46,23 +48,37 @@ def main():
     if args.get('--dont-keep-unreleased'):
         keep_unreleased = False
 
+    tfp = args['--tag-filter']
+    if tfp is None:
+        tfp = '*'
+
+    ib = args['--include-branches']
+    if ib is None:
+        ib = ['*']
+    else:
+        ib = ib.split(',')
+    eb = args['--exclude-branches']
+    if eb is None:
+        eb = []
+    else:
+        eb = eb.split(',')
+
     try:
         # Traverse the repository and group all commits to master by release
         tags, unreleased = traverse(args['--repo'], rev=args['--rev'],
-                                    keep_unreleased=keep_unreleased)
+                                    keep_unreleased=keep_unreleased,
+                                    tag_filter=tfp, include_branches=ib,
+                                    exclude_branches=eb)
     except ValueError as e:
         print('ERROR:', e)
         sys.exit(1)
-
-    tfp = args['--tag-filter']
-    new_tags = [x for x in tags if fnmatch.fnmatch(x.raw_name, tfp)]
 
     changelog = generate_changelog(
         template_dir=template_dir,
         title=args['--title'],
         description=args.get('--description'),
         unreleased=unreleased,
-        tags=new_tags)
+        tags=tags)
 
     # Get rid of some of those unnecessary newlines
     # changelog = changelog.replace('\n\n\n', '\n')
--- auto_changelog/parser.py
+++ auto_changelog/parser.py
@@ -5,6 +5,7 @@ the releases they correspond to.
 """
 
 import git
+import fnmatch
 
 from .models import Commit, Tag, Unreleased
 
@@ -23,23 +24,23 @@ def group_commits(tags, commits):
         # Everything is sorted in ascending order (earliest to most recent),
         # So everything before the first tag belongs to that one
         if index == 0:
-            children = filter(lambda c: c.date <= tag.date, commits)
+            for c in commits:
+                if c.date <= tag.date:
+                    tag.add_commit(c)
         else:
             prev_tag = tags[index - 1]
-            children = filter(lambda c: prev_tag.date < c.date <= tag.date,
-                              commits)
-
-        for child in children:
-            commits.remove(child)
-            tag.add_commit(child)
+            for c in commits:
+                if prev_tag.date < c.date <= tag.date:
+                    tag.add_commit(c)
 
     left_overs = list(filter(lambda c: c.date > tags[-1].date, commits))
     return left_overs
 
 
-def traverse(base_dir, rev='master', keep_unreleased=True):
+def traverse(base_dir, rev='master', keep_unreleased=True,
+        include_branches=['*'], exclude_branches=[], tag_filter='*'):
     repo = git.Repo(base_dir)
-    tags = repo.tags
+    tags = [x for x in repo.tags if fnmatch.fnmatch(x.name, tag_filter)]
 
     if len(tags) < 1:
         raise ValueError('Not enough tags to generate changelog')
@@ -55,9 +56,28 @@ def traverse(base_dir, rev='master', keep_unreleased=True):
     commits = list(repo.iter_commits(rev))
     commits = list(map(Commit, commits))  # Convert to Commit objects
 
+    new_commits = []
+    for c in commits:
+        branches_tmp = repo.git.branch('-a', '--contains', c.commit_hash)
+        add = False
+        branches = [x[2:].split()[0].replace('remotes/', '') for x in branches_tmp.splitlines()]
+        for b in branches:
+            for i in include_branches:
+                if fnmatch.fnmatch(b, i):
+                    add = True
+                    break
+        if add:
+            for b in branches:
+                for i in exclude_branches:
+                    if fnmatch.fnmatch(b, i):
+                        add = False
+                        break
+        if add:
+            new_commits.append(c)
+
     # Iterate through the commits, adding them to a tag's commit list
     # if it belongs to that release
-    left_overs = group_commits(wrapped_tags, commits)
+    left_overs = group_commits(wrapped_tags, new_commits)
 
     # If there are any left over commits (i.e. commits created since
     # the last release
