1e5bae6508cf4c01c9d1b8482c5802e3bac5475c
metworkbot <marty.fabien@orange.fr>
2019-04-18 11:05:58 +0200

    refactor: some flake8 fixes

--- auto_changelog/parser.py
+++ auto_changelog/parser.py
@@ -9,7 +9,6 @@ import git
 from .models import Commit, Tag, Unreleased
 
 
-
 def group_commits(tags, commits):
     tags = sorted(tags, key=lambda t: t.date)
 
@@ -19,20 +18,21 @@ def group_commits(tags, commits):
     # Sort the commits and filter out those not formatted correctly
     commits = sorted(commits, key=lambda c: c.date)
     commits = list(filter(lambda c: c.category, commits))
-    
+
     for index, tag in enumerate(tags):
-        # Everything is sorted in ascending order (earliest to most recent), 
+        # Everything is sorted in ascending order (earliest to most recent),
         # So everything before the first tag belongs to that one
         if index == 0:
             children = filter(lambda c: c.date <= tag.date, commits)
         else:
-            prev_tag = tags[index-1]
-            children = filter(lambda c: prev_tag.date < c.date <= tag.date, commits)
-            
+            prev_tag = tags[index - 1]
+            children = filter(lambda c: prev_tag.date < c.date <= tag.date,
+                              commits)
+
         for child in children:
             commits.remove(child)
             tag.add_commit(child)
-            
+
     left_overs = list(filter(lambda c: c.date > tags[-1].date, commits))
     return left_overs
 
@@ -45,21 +45,21 @@ def traverse(base_dir):
         raise ValueError('Not enough tags to generate changelog')
 
     wrapped_tags = []
-    for tagref in tags:        
+    for tagref in tags:
         t = Tag(
-            name=tagref.name, 
-            date=tagref.commit.committed_date, 
+            name=tagref.name,
+            date=tagref.commit.committed_date,
             commit=tagref.commit)
         wrapped_tags.append(t)
-        
+
     commits = list(repo.iter_commits('master'))
-    commits = list(map(Commit, commits)) # Convert to Commit objects
+    commits = list(map(Commit, commits))  # Convert to Commit objects
 
     # Iterate through the commits, adding them to a tag's commit list
     # if it belongs to that release
     left_overs = group_commits(wrapped_tags, commits)
 
-    # If there are any left over commits (i.e. commits created since 
+    # If there are any left over commits (i.e. commits created since
     # the last release
     if left_overs:
         unreleased = Unreleased(left_overs)
@@ -67,4 +67,3 @@ def traverse(base_dir):
         unreleased = None
 
     return wrapped_tags, unreleased
-
