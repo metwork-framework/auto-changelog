95295fd5508106041f58af8179ac022b214ad691
thebaptiste <jean-baptiste.veslin@meteo.fr>
2021-03-12 17:43:33 +0100

    build: fix flake8 errors

--- auto_changelog/models.py
+++ auto_changelog/models.py
@@ -10,22 +10,24 @@ from collections import defaultdict
 class Tag:
     def __init__(self, name, date, commit):
         self.raw_name = name
-        self.name = 'Version ' + name if not name.lower().startswith('v') else name
+        self.name = 'Version ' +
+            name if not name.lower().startswith('v') else name
         self._commit = commit
         self.date = datetime.datetime.fromtimestamp(date)
         self.commits = []
         self.groups = defaultdict(list)
-        
+ 
     def add_commit(self, commit):
         self.commits.append(commit)
         commit.tag = self
         self.groups[commit.category].append(commit)
-        
+
     def __repr__(self):
         return '<{}: {!r}>'.format(
                 self.__class__.__name__,
                 self.name)
-    
+
+
 class Unreleased:
     def __init__(self, commits):
         self.name = 'Unreleased'
@@ -34,15 +36,16 @@ class Unreleased:
         
         for commit in commits:
             self.add_commit(commit)
-            
+
     def add_commit(self, commit):
         self.groups[commit.category].append(commit)
 
     def __repr__(self):
         return '<{}: {!r}>'.format(
                 self.__class__.__name__,
-                self.name)        
-        
+                self.name)
+
+
 class Commit:
     def __init__(self, commit):
         self._commit = commit
@@ -53,20 +56,20 @@ class Commit:
         self.first_line = first_line
         self.message = commit.message
         self.tag = None
-        
+
         self.category, self.specific, self.description = self.categorize()
-        
+
     def categorize(self):
         match = re.match(r'(\w+)(\(\w+\))?:\s*(.*)', self.first_line)
-        
+
         if match:
             category, specific, description = match.groups()
             specific = specific[1:-1]  if specific else None # Remove surrounding brackets
             return category, specific, description
         else:
             return None, None, None
-        
-        
+
+
     def __repr__(self):
         return '<{}: {} "{}">'.format(
                 self.__class__.__name__,
